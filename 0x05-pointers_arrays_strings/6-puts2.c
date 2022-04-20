421941018eddeaa76a014c0e6451ee1b0543862f
Eyob <eyobaa3@gmail.com>
2022-04-20 02:54:32 -0700

    tasks

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -18,3 +18,18 @@ int _strlen(char *s)
 
 	return (len);
 }
+
+/**
+ * puts2 - prints every second character of a string, followed by a new line
+ * @str: string to print
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0' && i < _strlen(str); i += 2)
+		_putchar(str[i]);
+
+	_putchar('\n');
+}
